d45b01d064fd5e0c4c463be6f8435f5aee68dbfe
CasualX <CasualX@GitHub.com>
2017-04-16 17:17:39 +0200

    Work a bit on the Rect API.

--- src/rect.rs
+++ src/rect.rs
@@ -4,6 +4,8 @@ Rectangle and Box.
 
 use ::point::{Point2, Point3};
 
+use ::num::{Scalar};
+
 #[derive(Copy, Clone, Debug, Default, Eq, PartialEq)]
 #[repr(C)]
 pub struct Rect<T> {
@@ -25,6 +27,31 @@ macro_rules! rect {
 				$ty {
 					mins: mins,
 					maxs: maxs,
+				}
+			}
+			pub fn point(pt: $pt<T>) -> $ty<T> where T: Copy {
+				$ty {
+					mins: pt,
+					maxs: pt,
+				}
+			}
+		}
+
+		//----------------------------------------------------------------
+		// Operations
+
+		impl<T> $ty<T> {
+			pub fn intersect(self, rhs: $ty<T>) -> Option<$ty<T>> where T: Scalar {
+				let mins = $pt::max(self.mins, rhs.mins);
+				let maxs = $pt::min(self.maxs, rhs.maxs);
+				if mins <= maxs {
+					Some($ty {
+						mins: mins,
+						maxs: maxs,
+					})
+				}
+				else {
+					None
 				}
 			}
 		}
